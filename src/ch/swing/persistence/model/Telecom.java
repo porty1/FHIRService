07af3927f85021c4ec8805436437128668593970
porty1 <porty1@bfh.ch>
2017-04-22 13:24:41 +0200

    adapted javadoc

--- src/ch/swing/persistence/model/Telecom.java
+++ src/ch/swing/persistence/model/Telecom.java
@@ -2,6 +2,12 @@ package ch.swing.persistence.model;
 
 import java.util.Date;
 
+/**
+ * Model class which represents the telecom on the database
+ * 
+ * @author Yannis Portmann / Shpend Vladi
+ *
+ */
 public class Telecom {
 
 	private int telecomId;
@@ -11,49 +17,61 @@ public class Telecom {
 	private Date creationDate;
 	private Date lastUpdate;
 	private Date deletionDate;
-	
+
 	public int getTelecomId() {
 		return telecomId;
 	}
+
 	public void setTelecomId(int telecomId) {
 		this.telecomId = telecomId;
 	}
+
 	public int getSystem() {
 		return system;
 	}
+
 	public void setSystem(int system) {
 		this.system = system;
 	}
+
 	public int getValue() {
 		return value;
 	}
+
 	public void setValue(int value) {
 		this.value = value;
 	}
+
 	public Date getTelecomUse() {
 		return telecomUse;
 	}
+
 	public void setTelecomUse(Date telecomUse) {
 		this.telecomUse = telecomUse;
 	}
+
 	public Date getCreationDate() {
 		return creationDate;
 	}
+
 	public void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
 	}
+
 	public Date getLastUpdate() {
 		return lastUpdate;
 	}
+
 	public void setLastUpdate(Date lastUpdate) {
 		this.lastUpdate = lastUpdate;
 	}
+
 	public Date getDeletionDate() {
 		return deletionDate;
 	}
+
 	public void setDeletionDate(Date deletionDate) {
 		this.deletionDate = deletionDate;
 	}
-	
-	
+
 }
