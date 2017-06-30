cd215e6570e0092ee1ee70171381f2dbaba8fb64
porty1 <porty1@bfh.ch>
2017-06-30 10:30:54 +0200

    FinalVersion
    q

--- src/ch/swing/bc/FHIRServiceScheduler.java
+++ src/ch/swing/bc/FHIRServiceScheduler.java
@@ -27,47 +27,21 @@ public final class FHIRServiceScheduler {
 		// ObservationConverter.getInstance().startObservationConverter();
 		// MedicationConverter.getInstance().startMedicationConverter();
 
-		Runnable runnableMasterData = new Runnable() {
+		Runnable runnableFHIRService = new Runnable() {
 			@Override
 			public void run() {
 				try {
 					MasterdataConverter.getInstance().startMasterdataConverter();
-				} catch (FHIRServiceException | FHIRException e) {
-					logger.error(e.getMessage());
-				}
-			}
-		};
-		Runnable runnableObservation = new Runnable() {
-			@Override
-			public void run() {
-				try {
-					ObservationConverter.getInstance().startObservationConverter();
-				} catch (FHIRServiceException | FHIRException e) {
-					logger.error(e.getMessage());
-				}
-			}
-		};
-		Runnable runnableNursingReport = new Runnable() {
-			@Override
-			public void run() {
-				try {
 					ObservationConverter.getInstance().startNursingReportConverter();
+					ObservationConverter.getInstance().startObservationConverter();
+					MedicationConverter.getInstance().startMedicationConverter();
+
 				} catch (FHIRServiceException | FHIRException e) {
 					logger.error(e.getMessage());
 				}
 			}
 		};
-		Runnable runnableMedication = new Runnable() {
-			public void run() {
-				MedicationConverter.getInstance().startMedicationConverter();
-			}
-		};
-		ScheduledExecutorService service = Executors.newScheduledThreadPool(4);
-		// ScheduledExecutorService service =
-		// Executors.newSingleThreadScheduledExecutor();
-		service.scheduleAtFixedRate(runnableMasterData, 0, Configuration.SCHEDULERMINUTES, TimeUnit.MINUTES);
-		service.scheduleAtFixedRate(runnableNursingReport, 0, Configuration.SCHEDULERMINUTES, TimeUnit.MINUTES);
-		service.scheduleAtFixedRate(runnableObservation, 0, Configuration.SCHEDULERMINUTES, TimeUnit.MINUTES);
-		service.scheduleAtFixedRate(runnableMedication, 0, Configuration.SCHEDULERMINUTESMEDICATION, TimeUnit.MINUTES);
+		ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+		service.scheduleAtFixedRate(runnableFHIRService, 0, Configuration.SCHEDULERMINUTES, TimeUnit.MINUTES);
 	}
 }
